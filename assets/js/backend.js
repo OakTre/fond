8fb48670e8a83cb88a31fb6f343c581ace63bd35
Artem Safin <artemsafin.webdev@gmail.com>
2024-06-01 20:06:55 +0300

    Updates

--- assets/js/backend.js
+++ assets/js/backend.js
@@ -1,3 +1,18 @@
 document.addEventListener("DOMContentLoaded", (event) => {
-  //
+  const gallery = document.querySelector(".care__slider");
+
+  if (!gallery) return;
+  // new Swiper(gallery, {
+  //   spaceBetween: 8,
+  //   slidesPerView: "auto",
+
+  //   allowTouchMove: false,
+  //   loop: true,
+
+  //   speed: 3000,
+  //   autoplay: {
+  //     delay: 0,
+  //     disableOnInteraction: true,
+  //   },
+  // });
 });
